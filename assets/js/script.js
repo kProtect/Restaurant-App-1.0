d84b2e0ad203ea8b4140d68fddb0900a296e3fdf
Happy Dog <asierramejia33@gmail.com>
2022-04-09 14:02:13 -0400

    Andres - Commented to add image element on js

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,24 +7,23 @@ var nthComment = 1;
 function makeComment() {}
 
 function makeReview() {
-    for(var i = 0; i <=nthComment-1; i++){
+    for (var i = 0; i <= nthComment - 1; i++) {
         reviewEl.append();
         nthComment += 1;
     }
 
 }
 
-function getRandomImage()
-{
-  // get the data from the server
-  $.get( "https://dog.ceo/api/breeds/image/random", function( data ) {
-    // set the source of the image
-    $('#dogImage').attr('src', data.message)
-  });
+function getRandomImage() {
+    // get the data from the server
+    $.get("https://dog.ceo/api/breeds/image/random", function(data) {
+        // set the source of the image
+        $('#dogImage').attr('src', data.message)
+    });
 
-  $.get("www.thecocktaildb.com/api/json/v1/1/random.php", function(data) {
-      $('#cocktailImage').attr('src', data.message)
-  })
+    $.get("www.thecocktaildb.com/api/json/v1/1/random.php", function(data) {
+        $('#cocktailImage').attr('src', data.message)
+    })
 
 }
 
@@ -32,13 +31,14 @@ $("#submit").on("click", function() {
     var userText = $(this).siblings("textarea").val();
     var userStar = $(this).siblings("textarea").attr();
     localStorage.setItem(userText, userStar);
-   getRandomImage(); // API generates a random image of a dog.
+    // Andres_Note - Need to create couple of image element to put in there the images for the dog and the cocktail..
+    getRandomImage(); // API generates a random image of a dog.
 })
 
 
 
-for(var i=0; i<nthComment; i++){
+for (var i = 0; i < nthComment; i++) {
 
     $(`#comment${i}`).val(localStorage.getItem(`comment${i}`)); // looping through Local Storage to save user values to the page after refresh.
-    
-    }
+
+}
