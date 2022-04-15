cdb1aae7d8d501319c83c0810ef45da3df4065a6
Malik Henry <mhenry30658@gmail.com>
2022-04-14 21:10:18 -0400

    save comments to screen

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,13 +4,23 @@ var starEl = $('#star')
 var reviewEl = $('#review')
 var nthComment = 1;
  
-function makeComment() {}
+function showComment() {
+   var reviews = JSON.parse(window.localStorage.getItem('reviews')) || []
+   reviews.forEach(function (comment) {
+      reviewEl.append(`<div class="box">`+ comment.userText + `</div>`)
+   });
+}
+
+showComment();
  
 function makeReview(userText, userStar) {
    for(var i = 0; i <nthComment; i++){
-       reviewEl.append(`<div class="box">`+ userText + `</div>`);     
+       reviewEl.append(`<div class="box">`+ userText + `</div>`);
+       reviewEl.append(`<div class="box">`+ userStar + `</div>`);     
    }
 }
+
+
  
 $("#submit").on("click", function() {
    var userText = $("#comment").val();
