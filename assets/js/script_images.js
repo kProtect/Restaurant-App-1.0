a6a63eaa8997adafae0c6c36ee273faf7cf2bf38
Happy Dog <asierramejia33@gmail.com>
2022-04-16 09:53:52 -0400

    Andres - Random Appetizer API working

--- assets/js/script_images.js
+++ assets/js/script_images.js
@@ -1,65 +1,56 @@
 var bodyE = document.querySelector("body")
-var addDivDog = document.createElement("div")
-var addImgDog = document.createElement("img")
-var addPDog = document.createElement("p")
-
-
-function getRandomDog() {
-    // get the data from the server
-    $.get("https://dog.ceo/api/breeds/image/random", function(data) {
-        // set the source of the image
-        $('#dogImage').attr('src', data.message);
-        console.log("Random Dog Pic Retrieved:\n", data.message);
-    });
-
-};
-getRandomDog();
 
 function getDogFunction(event) {
-    event.preventDefault();
-    bodyE.append(addDivDog);
-    addDivDog.append(addImgDog);
-    addImgDog.setAttribute("class", "fit-picture");
-    addImgDog.setAttribute("alt", "HappyDog");
-    addImgDog.setAttribute("id", "dogImage");
-    addImgDog.setAttribute("src", getRandomDog());
-    bodyE.append(addPDog);
-    addPDog.setAttribute("style", "p");
-    addPDog.textContent = "Your Puppy is Welcome to Join You";
-};
+    var requestUrl = "https://dog.ceo/api/breeds/image/random";
+    fetch(requestUrl)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            var imgDog = data.message
+            console.log("imgDog:\n", imgDog);
+            var addDivDog = document.createElement("div")
+            var addImgDog = document.createElement("img")
+            var addPDog = document.createElement("p")
+            bodyE.append(addDivDog);
+            addDivDog.append(addImgDog);
+            addImgDog.setAttribute("class", "fit-picture");
+            addImgDog.setAttribute("alt", "HappyDog");
+            addImgDog.setAttribute("id", "dogImage");
+            addImgDog.setAttribute("src", imgDog);
+            bodyE.append(addPDog);
+            addPDog.setAttribute("style", "p");
+            addPDog.textContent = "Your Puppy is Welcome to Join You";
+        });
+}
 document.getElementById("submit").addEventListener("click", getDogFunction);
 
-var bodyE = document.querySelector("body")
-var addDivAppetizer = document.createElement("div")
-var addImgAppetizer = document.createElement("img")
-var addPAppetizer = document.createElement("p")
-const get_meal_btn = document.getElementById("submit");
-
-function getRandomAppetizer() {
-    get_meal_btn.addEventListener("click", () => {
-        fetch('https://www.themealdb.com/api/json/v1/1/random.php')
-            .then(res => res.json())
-            .then(res => {
-                createMeal(res.meal.strMealThumb);
-            })
-            .catch(e => {
-                console.warn(e);
-            });
-    });
-
-};
-getRandomAppetizer();
-
 function getAppetizerFunction(event) {
-    event.preventDefault();
-    bodyE.append(addDivAppetizer);
-    addDivAppetizer.append(addImgAppetizer);
-    addImgAppetizer.setAttribute("class", "fit-picture");
-    addImgAppetizer.setAttribute("alt", "NiceAppetizer");
-    addImgAppetizer.setAttribute("id", "appetizerImage");
-    addImgAppetizer.setAttribute("src", getRandomAppetizer());
-    bodyE.append(addPAppetizer);
-    addPAppetizer.setAttribute("style", "p");
-    addPAppetizer.textContent = "Thank you for your review. Please enjoy one of our Famous Appetizers on the house next time you visit us";
-};
-get_meal_btn.addEventListener("click", getAppetizerFunction);
+    var requestUrl = "https://www.themealdb.com/api/json/v1/1/random.php"
+    fetch(requestUrl)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            //console.log("meals:   \n", data.meals)
+            var mealsArray = data.meals
+                //console.log("meals array:   \n", mealsArray)
+            for (var i = 0; i < mealsArray.length; i++) {
+                var addDivAppetizer = document.createElement("div")
+                var addImgAppetizer = document.createElement("img");
+                var addPAppetizer = document.createElement("p")
+                imgAppetizer = mealsArray[i].strMealThumb;
+                console.log("imgAppetizer: \n", imgAppetizer)
+                bodyE.append(addDivAppetizer);
+                addDivAppetizer.append(addImgAppetizer);
+                addImgAppetizer.setAttribute("class", "fit-picture");
+                addImgAppetizer.setAttribute("alt", "NiceAppetizer");
+                addImgAppetizer.setAttribute("id", "appetizerImage");
+                addImgAppetizer.setAttribute("src", imgAppetizer);
+                bodyE.append(addPAppetizer);
+                addPAppetizer.setAttribute("style", "p");
+                addPAppetizer.textContent = "Thank you for your review. Please enjoy one of our Famous Appetizers on the house next time you visit us";
+            }
+        })
+}
+document.getElementById("submit").addEventListener("click", getAppetizerFunction);
