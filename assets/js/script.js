f0eb32f5ca5cab266d79aafedcebc4c405473792
unknown <kyujin0704@gmail.com>
2022-04-16 12:42:08 -0400

    final -lee

--- assets/js/script.js
+++ assets/js/script.js
@@ -58,12 +58,6 @@ $("#submit").on("click", function() {
  
 
 
-
-
-
-
-
-
 for(var i=0; i<nthComment; i++){
  
    $(`#comment${i}`).val(localStorage.getItem(`comment${i}`)); // looping through Local Storage to save user values to the page after refresh.
